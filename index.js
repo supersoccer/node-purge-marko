7477ec05f83401f642a1a4ff7cc75ede8f9b4db4
hkyo89 <hkyo89@yahoo.co.jp>
2018-07-20 01:15:11 +0700

    remove class for autostart

--- index.js
+++ index.js
@@ -2,24 +2,19 @@ const path = require('path')
 const chokidar = require('chokidar')
 const production = process.env.NODE_ENV === 'production'
 
-class PurgeMarko {
-  watch (baseDir) {
-    if (!production) {
-      baseDir = baseDir || '.'
-      const watchDir = path.resolve(baseDir, '**/*.marko')
-      const watcher = chokidar.watch(watchDir)
-      watcher.on('ready', () => {
-        watcher.on('all', (event, path) => {
-          Object.keys(require.cache).forEach((id) => {
-            if (id.indexOf('marko') >= 0) {
-              delete require.cache[id]
-            }
-          })
-          console.log(`[purge-marko] cache cleared`)
-        })
+if (!production) {
+  const prefix = '[purge-marko]'
+  console.log(`${prefix} watching`)
+  const watchDir = path.resolve('./**/*.marko')
+  const watcher = chokidar.watch(watchDir)
+  watcher.on('ready', () => {
+    watcher.on('all', (event, path) => {
+      Object.keys(require.cache).forEach((id) => {
+        if (id.indexOf('marko') >= 0) {
+            delete require.cache[id]
+        }
       })
-    }
-  }
+      console.log(`${prefix} cache cleared`)
+    })
+  })
 }
-
-module.exports = new PurgeMarko()
